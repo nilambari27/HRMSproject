8a34319754149448500cac36fdf8d0a39fdf5152
nilambari27 <kuntenilambari27@gmail.com>
2023-08-09 15:47:13 +0530

    master page changes

--- HRMSWeb/Company_Details.aspx
+++ HRMSWeb/Company_Details.aspx
@@ -2,9 +2,8 @@
 <asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
-
-
-    <asp:Label ID="Label1" runat="server" Text="Company Profile" Font-Bold="True" Font-Names="Copperplate Gothic Bold" Font-Size="X-Large"></asp:Label>
+    
+    <asp:Label runat="server" Text="Company Profile" Font-Bold="True" Font-Names="Copperplate Gothic Bold" Font-Size="X-Large"></asp:Label>
 
 
 </asp:Content>
---
+++ HRMSWeb/Scripts/Master.js
@@ -0,0 +1,15 @@
+﻿// JavaScript to toggle the dropdown
+document.addEventListener('DOMContentLoaded', function () {
+    var dropdownToggles = document.querySelectorAll('.nav-table > ul > li > a');
+    dropdownToggles.forEach(function (toggle) {
+        toggle.addEventListener('click', function (event) {
+            event.preventDefault();
+            var dropdown = this.nextElementSibling;
+            if (dropdown.style.display === 'block') {
+                dropdown.style.display = 'none';
+            } else {
+                dropdown.style.display = 'block';
+            }
+        });
+    });
+});
